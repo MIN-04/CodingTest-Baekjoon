93ca1850f3d6547f6a80b94fab2981c6eb000ac0
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-09-23 20:53:58 +0900

    20.09.23 [Greedy] Bronze02 - 5585. 거스름돈

---
+++ Tag/Greedy/No5585.java
@@ -0,0 +1,30 @@
+package tag.greedy;
+
+import java.util.Scanner;
+
+public class No5585 {
+	
+	/**
+	 	[Greedy] Bronze02 - 5585. °Å½º¸§µ·
+	 */
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//ÁöºÒÇØ¾ßÇÏ´Â µ·
+		int price = sc.nextInt();
+		//°Å½º¸§µ· ¹è¿­
+		int[] money = {500,100,50,10,5,1};
+		//°Å½½·¯Áà¾ßÇÒ µ·
+		int change = 1000 - price;
+		//°Å½º¸§µ· °¹¼ö
+		int cnt = 0;
+		
+		for(int i=0; i<6; i++) {
+			cnt += (change / money[i]);
+			change %= money[i];
+			if(change == 0) break;
+		}
+		
+		System.out.print(cnt);
+	}
+}
