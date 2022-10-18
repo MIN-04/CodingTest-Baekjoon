c85000a661c359332a42c076112932d77ee85133
Min-Ji Kim <62365297+MIN-04@users.noreply.github.com>
2022-10-18 20:25:39 +0900

    Create no2798.py

---
+++ Tag/Array/no2798.py
@@ -0,0 +1,13 @@
+# 브론즈2 - 2798. 블랙잭 (https://www.acmicpc.net/problem/2798)
+
+n, m = list(map(int, input().split(' ')))
+data = list(map(int, input().split(' ')))
+length = len(data)
+result = 0
+for i in range(0, length):
+    for j in range(i+1, length):
+        for k in range(j+1, length):
+            sum = data[i] + data[j] + data[k]
+            if sum <= m:
+                result = max(result, sum)
+print(result)
