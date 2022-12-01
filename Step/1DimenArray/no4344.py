5071c6c853da648065a5962ba331584586791056
Min-Ji Kim <62365297+MIN-04@users.noreply.github.com>
2022-12-01 21:30:22 +0900

    Create no4344.py

---
+++ Step/1DimenArray/no4344.py
@@ -0,0 +1,13 @@
+# Bronze 1 - 4344. OX퀴즈 (https://www.acmicpc.net/problem/4344)
+
+for _ in range(int(input())):
+    scores = list(map(int, input().split(" ")))
+    avgs = sum(scores[1:]) / scores[0]
+    
+    cnt = 0
+    for score in scores[1:]:
+        if score > avgs:
+            cnt += 1
+    
+    rate = cnt / scores[0] * 100
+    print(f'{rate:.3f}%')
